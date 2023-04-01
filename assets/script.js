20e9f5835d1921d2ef769c6efcdf6b7250605980
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-03-31 18:38:15 -0700

    change html file

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,17 @@
 var wordsToChoosefrom = ['сat', 'dog', 'parrot']
+var secretWord =  document.querySelector(".blanks");
+var startButton = document.querySelector(".start-game");
 
-var startButton = document.querySelector("start-game");
 
-function creareRnmNumber()
 
 function startTheGame() {
+    var wordToGuess  = wordsToChoosefrom[Math.floor(Math.random() * wordsToChoosefrom.length)];
+    var blanks = []
+    for (var i = 0;i<wordToGuess.length; i++){
 
+        blanks.push("_");
+    }
+    secretWord.textContent = blanks;
 }
 
 
