8ade3a5defc22ee33e3fffc551077101b7fd0af1
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-03-31 21:00:21 -0700

    add start button event listener and Guess the letter function

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,45 @@ var wordsToChoosefrom = ['сat', 'dog', 'parrot']
 var secretWord =  document.querySelector(".blanks");
 var startButton = document.querySelector(".start-game");
 
+var blanks = [];
+
 var lettersToGuess = [];
+var gameStarted = false;
 function startTheGame() {
+    gameStarted = true;
     secretWord.textContent = ""
     var wordToGuess  = wordsToChoosefrom[Math.floor(Math.random() * wordsToChoosefrom.length)];
     console.log(wordToGuess)
     lettersToGuess = wordToGuess.split("");
-    var blanks = "";
+    
+    var display = "";
     for (var i = 0;i<lettersToGuess.length; i++){
-        blanks += "_ ";
+        blanks.push("_");
+        display += "_ ";
     }
-    secretWord.textContent = blanks;
+    secretWord.textContent = display;
 }
 
 startButton.addEventListener("click", startTheGame );
 
 
 function GuessTheLetter(event){
+    console.log("hello!")
     var keyPress = event.key;
-
-
+    if (lettersToGuess.includes(keyPress))
+    {
+        for (var i = 0; i < lettersToGuess.length; i++) {
+            if ( lettersToGuess[i] ===  keyPress) {
+                blanks[i] = keyPress;
+              }
+        }
+        var display = "";
+        for (var i = 0;i<lettersToGuess.length; i++){
+            display +=  blanks[i] + " ";
+        }
+        secretWord.textContent = display;
+        
+    }
 }
 
 document.addEventListener("keyup", GuessTheLetter);
