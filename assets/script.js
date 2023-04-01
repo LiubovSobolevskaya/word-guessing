d94af93a6c157ed635e2a806d799cc0ef92d1fc8
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-03-31 18:24:57 -0700

    change html file

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var wordsToChoosefrom = ['сat', 'dog', 'parrot']
+
+var startButton = document.querySelector("start-game");
+
+function creareRnmNumber()
+
+function startTheGame() {
+
+}
+
+
+startButton.addEventListener("click", startTheGame() );
