3960a79c3e7ab910de2689b12dc1e982b90d69bb
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-03-31 20:45:22 -0700

    add initial code to java script file

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,26 @@ var wordsToChoosefrom = ['сat', 'dog', 'parrot']
 var secretWord =  document.querySelector(".blanks");
 var startButton = document.querySelector(".start-game");
 
-
-
+var lettersToGuess = [];
 function startTheGame() {
+    secretWord.textContent = ""
     var wordToGuess  = wordsToChoosefrom[Math.floor(Math.random() * wordsToChoosefrom.length)];
     console.log(wordToGuess)
-    var lettersToGuess = wordToGuess.split("");
-    var blanks = []
+    lettersToGuess = wordToGuess.split("");
+    var blanks = "";
     for (var i = 0;i<lettersToGuess.length; i++){
-
-        blanks.push("_");
+        blanks += "_ ";
     }
     secretWord.textContent = blanks;
 }
 
 startButton.addEventListener("click", startTheGame );
+
+
+function GuessTheLetter(event){
+    var keyPress = event.key;
+
+
+}
+
+document.addEventListener("keyup", GuessTheLetter);
